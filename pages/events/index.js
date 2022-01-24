42172f0d48662a5582ce7441670a285ac9ea3d4d
Tettei Shahday-Annang <tetteis@gmail.com>
2022-01-24 16:00:04 +0000

    Adding initial files

---
+++ pages/events/index.js
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout";
+
+export default function EventsPage() {
+  return (
+    <Layout>
+      <h1>My Events</h1>
+      <h2>Kill them all</h2>
+    </Layout>
+  );
+}
